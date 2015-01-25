0850a9c2b04b222efa3a58eacd926ac7b8ea8814
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-01-25 17:25:42 +0000

    Initial commit

---
+++ src/CodeFlow.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Pierre on 25/01/2015.
+ */
+public class CodeFlow {
+
+    public void launch(int userInput) {
+        List<Integer> intList = new ArrayList<Integer>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+        intList.add(5);
+        intList.add(6);
+        try {
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            } catch (IndexOutOfBoundsException ex) {
+            ex.printStackTrace();
+            }
+        }
+
+}
